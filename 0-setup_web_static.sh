2f6a774de9f6336ae365bf0fd74a0861c180a654
Ngwanatuka <Ngwanatuka@gmail.com>
2023-05-07 19:04:17 +0200

    fixing code

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,8 +3,8 @@
 
 # Install Nginx if not already installed
 if ! command -v nginx >/dev/null 2>&1; then
-    apt-get update
-    apt-get install -y nginx
+    sudo apt-get update
+    sudo apt-get install -y nginx
 fi
 
 # Create necessary directories if they don't exist
@@ -19,16 +19,17 @@ touch "$test_dir/index.html"
 # Create or recreate symbolic link
 current_link="/data/web_static/current"
 if [ -L "$current_link" ]; then
-    rm "$current_link"
+    sudo rm "$current_link"
 fi
-ln -s "$test_dir" "$current_link"
+sudo ln -s "$test_dir" "$current_link"
 
 # Set ownership of /data/ folder recursively
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update Nginx configuration
 config_file="/etc/nginx/sites-available/default"
-sed -i '/^\tlocation \/ {$/a'"$'\n''\t\tlocation /hbnb_static/ {\n\t\t\talias /data/web_static/current/;\n\t\t}'" "$config_file"
+#setting up the page to be served
+sudo sed -i '/server_name _;/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' "$config_file"
 
 # Restart Nginx to apply changes
-service nginx restart
+sudo service nginx restart
