8a9e35cb6f8f029918682f7e75616a194bab972e
Ngwanatuka <Ngwanatuka@gmail.com>
2023-05-19 22:36:35 +0200

    a script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This script starts a Flask web application with
+specific routes
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
